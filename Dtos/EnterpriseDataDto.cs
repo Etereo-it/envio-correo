39e94ed33fb40e92107c68ad440b5488628fd81b
Luis Tolentino <luist@4waystech.com>
2023-05-01 09:29:44 -0500

    Migracion de proyecto

---
+++ Dtos/EnterpriseDataDto.cs
@@ -0,0 +1,16 @@
+﻿namespace envioMasivoCorreos.Dtos
+{
+    public class EnterpriseDataDto
+    {
+        public string RUC { get; set; }
+        public string Email { get; set; }
+        public string EmailADM { get; set; }
+        public string Phone { get; set; }
+        public string Departament { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string BusinessName { get; set; }
+        public string Heading { get; set; }
+        public string WebPage { get; set; }
+    }
+}
